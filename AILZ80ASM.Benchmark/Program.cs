c054a21fdd2c3d50a548928e1d4be063cd47a34b
AILight <ishino@ailight.jp>
2022-03-06 18:50:18 +0900

    ベンチマークの準備

---
+++ AILZ80ASM.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace AILZ80ASM.Benshmark
+{
+    public class Program
+    {
+        public static int Main(params string[] args)
+        {
+            var result = AILZ80ASM.Program.Main(@"./Main.Z80", @"-cd", @"./TestSource/Benchmark1");
+
+            return 0;
+        }
+    }
+}
---
+++ AILZ80ASM.Benchmark/README.md
@@ -0,0 +1,11 @@
+# Z80 Assembler 'AILZ80ASM' Benchmark
+
+## ベンチマークプロジェクトとは？
+- パフォーマンスチューニングを行うときに使うプロジェクト
+- 機能を追加する事で速度が低下していないかを確認する為に利用
+
+## 謝辞
+- AILZ80ASMをお使いの皆様
+
+## 利用ライブラリ
+- [BenchmarkDotNet](https://benchmarkdotnet.org/)
