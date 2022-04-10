ea6ad349b8effddbf2a09cc0091eed09eb861ac0
AILight <ishino@ailight.jp>
2022-04-10 17:41:21 +0900

    相対ジャンプのエラー時を修正した

--- AILZ80ASM/Assembler/Error.cs
+++ AILZ80ASM/Assembler/Error.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AILZ80ASM.Assembler
 {
@@ -136,7 +137,7 @@ namespace AILZ80ASM.Assembler
 
             [ErrorCodeEnum.E0001] = "無効な命令が指定されました。",
             [ErrorCodeEnum.E0002] = "バイト変換で有効桁数をオーバーしました。",
-            [ErrorCodeEnum.E0003] = $"相対ジャンプの範囲違反、有効範囲は{byte.MinValue}～{byte.MaxValue}までです。",
+            [ErrorCodeEnum.E0003] = $"相対ジャンプの範囲違反、有効範囲は{SByte.MinValue}～{SByte.MaxValue}までです。",
             [ErrorCodeEnum.E0004] = "演算、もしくはラベルの解決に失敗しました。定義を確認してください。[{0}]",
             [ErrorCodeEnum.E0005] = "16進数の変換に失敗しました。",
             [ErrorCodeEnum.E0006] = "10進数の変換に失敗しました。",
